3c1ed29b836cc99504f5c395ecc64a102a613f05
Danilo Araújo Silva <silva.danilo.araujo@gmail.com>
2017-06-15 15:25:24 -0300

    Improving a map test.

--- source/frontend/javascript/react/react_01/src/model/helper/immutable/map.test.js
+++ source/frontend/javascript/react/react_01/src/model/helper/immutable/map.test.js
@@ -2,6 +2,15 @@
  * Immutable map helper test.
  */
 
+import {Map} from 'immutable'
+import {get, set} from './map'
+
 it('can be required without crashing', () => {
 	require('./map')
+})
+
+it('can set and get a value on a mutable map', () => {
+	const map = new Map().asMutable()
+	set(map, 'a.b.c.d.e', 1)
+	expect(get(map, 'a.b.c.d.e')).toBe(1)
 })
