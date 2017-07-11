68604c07ed2c8146fbae14c4620189941494e332
Danilo Araújo Silva <danilo.silva@peixeurbano.com>
2017-07-11 13:59:44 -0300

    Improving to work with windows. Replacing symlinks, adding cross-env, adding normalize.css.

--- source/frontend/javascript/react/react_01/src/index.js
+++ source/frontend/javascript/react/react_01/src/index.js
@@ -1,1 +1,16 @@
-controller/root.controller.js
+/**
+ * Root controller.
+ * The react application starts here.
+ */
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import Root from 'model/component/root'
+import registerServiceWorker from 'model/initializer/register-service-worker'
+
+ReactDOM.render(
+  <Root />,
+  document.getElementById('root')
+)
+
+registerServiceWorker()
